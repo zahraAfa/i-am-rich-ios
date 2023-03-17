15dd89e86437622b3c25f18e97d8dd01aa0c259d
Anisa Zahra <anisafatimahazzahra@gmail.com>
2023-03-17 16:50:05 +0700

    Initial Commit

---
+++ I am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by Anisa Fatimah Azzahra on 17/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
